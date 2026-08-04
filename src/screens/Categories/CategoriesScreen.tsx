153038d70076876fa0debb0d4c36cd24ebf77f19
bizzkoot <bizz_koot@yahoo.com>
2026-08-04 19:21:33 +0800

    fix(categories): stable key for drag reordering

--- src/screens/Categories/CategoriesScreen.tsx
+++ src/screens/Categories/CategoriesScreen.tsx
@@ -91,7 +91,7 @@ const CategoriesScreen = () => {
           data={userCategories}
           contentContainerStyle={styles.contentCtn}
           renderItem={renderItem}
-          keyExtractor={(item, index) => `${item.id}-${index}`}
+          keyExtractor={item => item.id.toString()}
           onDragEnd={onDragEnd}
           activationDistance={10}
           autoscrollSpeed={100}
